b19899d7fa695e587b18c3853fd01166742f7f93
immerfallen <marodemelo10@gmail.com>
2023-11-07 20:01:28 -0300

    Indexadores de classe

---
+++ DesignPatterns/Indexadores.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    public class Indexadores
+    {
+        private List<Indexadores> _indexadores = new List<Indexadores>();
+        public string Mensagem { get; set; }
+
+        public Indexadores(string mensagem = "")
+        {
+            if (!string.IsNullOrEmpty(mensagem))
+            {
+                Mensagem = mensagem;
+            }
+        }
+
+        public Indexadores this[int index]
+        {
+            get
+            {
+                return _indexadores[index];
+            }
+            set
+            {
+                _indexadores.Add(value);
+            }
+        }
+
+    }
+}
+
--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -6,7 +6,18 @@ namespace DesignPatterns
     {
         static void Main(string[] args)
         {
-            
+            Indexadores indexador = new Indexadores();
+            int[] lista = { 1, 2, 3, 4 };
+
+            for (int i = 0; i < lista.Length; i++)
+            {
+                indexador[i] = new Indexadores($"Indexador {i}");
+            }
+
+            for (int i = 0; i < lista.Length; i++)
+            {
+                Console.WriteLine(indexador[i].Mensagem);
+            }
 
         }
     }
