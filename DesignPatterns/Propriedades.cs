7948cb8220b8a332bb66eab6419492b63f004222
immerfallen <marodemelo10@gmail.com>
2023-11-07 19:52:19 -0300

    Add project files.

---
+++ DesignPatterns/Propriedades.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    public class Propriedades
+    {
+        private double _segundos;
+
+        public double Horas
+        {
+            set
+            {
+                if(value < 0 || value > 24)
+                {
+                    Console.WriteLine("Valor inválido...");
+                }
+                else
+                {
+                    _segundos = value*3600;
+                }
+            }
+            get
+            {
+                return _segundos / 3600;
+            }
+        }
+    }
+}
